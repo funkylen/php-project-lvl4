ea4c75571c7f8cd1b9ad7d447c5f8e10e749e788
funkylen <funkylen@gmail.com>
2023-04-30 00:49:08 +0300

    fix

--- resources/views/task_status/index.blade.php
+++ resources/views/task_status/index.blade.php
@@ -24,7 +24,7 @@
                         @foreach($models as $model)
 
                             <tr>
-                                <th scope="row">{{ $model->id }}</th>
+                                <td>{{ $model->id }}</td>
                                 <td>{{ $model->name }}</td>
                                 <td>{{ $model->created_at }}</td>
                                 @auth
