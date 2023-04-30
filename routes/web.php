8d4a661b7b1a8b13ca60538e6944d4fada6f99d3
funkylen <funkylen@gmail.com>
2023-04-30 05:05:22 +0300

    fixes

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,9 @@
 <?php
 
 use App\Http\Controllers\LabelController;
-use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\TaskController;
 use App\Http\Controllers\TaskStatusController;
 use Illuminate\Support\Facades\Route;
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
 
 require __DIR__ . '/auth.php';
 
